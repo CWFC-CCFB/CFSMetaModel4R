7782832b242fc3e8ed94f4a922a8a59788a13af1
jflavoie33 <atmailjfl@gmail.com>
2022-12-23 11:47:56 -0500

    Added OSMClass and OSMOutputRequestList classes implementation Fixed OSMExample accordingly

--- R/OSMClass.R
+++ R/OSMClass.R
@@ -1,21 +1,112 @@
 #'
-#' Input data for OSM
+#' Constructor for the OSMClass class.
 #'
-#' @docType data
+#' @description This class is the interface to the OSM http server.
 #'
-#' @usage data(OSMThreeStandList)
+#' @return an S3 OSMClass instance
 #'
-#' @keywords datasets
+#'  TODO update documentation here
 #'
-#' @examples
-#' data(OSMThreeStandList)
+#' @details
+#'
+#' The class contains the following methods: \cr
+#' \itemize{
+#'
+#' \item \bold{ConvertDataFrameToCSVString(dataFrameInstance)} \cr
+#' Utility method to convert a dataframe to CSV string to be sent as input data to the server \cr
+#' \item dataFrameInstance - The status class to be requested (character, typically one of "Alive", "Dead" )
+#' \item variable - The variable to be requested (character, typically one of "Volume", "Biomass")
+#' \item aggregrationPatterns - A list of aggregation patterns to be used for the request (named List where the names are the aggregation groups, and the list data are the species)
+#'
+#'
+#' \item \bold{Simulate(data, outputRequestList, variant, years, ypc)} \cr
+#' Converts the current OSMOutputRequestList to a json string \cr
+#' \item data - a string in CSV format that represents the input data to run the simulation on
+#' \item outputRequestList - An object of type OSMOutputRequestList that contains the output data requested
+#' \item variant - A string containing the variant name to use for simulation
+#' \item years - An int containing the number of years the simulation should use
+#' \item ypc - An int containing the number of years per cycle the simulation should use
+
+#' Return character vector
+#' }
+#'
+#'data
 "OSMThreeStandList"
 
-
 #' @export
-convertDataFrameToCSVString <- function(dataFrameInstance) {
-  outputVector <- sapply(1:nrow(dataFrameInstance), function(i) {paste(dataFrameInstance[i,], collapse= ",")})
-  outputVector <- c(paste(colnames(dataFrameInstance), collapse= ","), outputVector)
-  outputString <- paste(outputVector, collapse = "\r\n")
-  return(outputString)
+new_OSMClass <- function(host) {
+  me <- new.env(parent = emptyenv())
+  class(me) <- c("OSMClass")
+  me$host <- host
+  me$orList <- list()
+  delayedAssign("ConvertDataFrameToCSVString",
+                function(dataFrameInstance) {
+                  outputVector <- sapply(1:nrow(dataFrameInstance), function(i) {paste(dataFrameInstance[i,], collapse= ",")})
+                  outputVector <- c(paste(colnames(dataFrameInstance), collapse= ","), outputVector)
+                  outputString <- paste(outputVector, collapse = "\r\n")
+                  return(outputString)
+                },
+                assign.env = me)
+
+  delayedAssign("Simulate",
+                function(data, outputRequestList, variant, years, ypc) {
+                  outputRequestListJSON <- outputRequestList$toJSONString()
+                  csvData <- me$ConvertDataFrameToCSVString(data)
+                  url <- paste(me$host, "OSMSimulation/Simulate", sep="/")
+                  r <- POST( url, query = list(years = as.character(years), variant = variant, ypc = as.character(ypc)), body = list(data=csvData, output=outputRequestListJSON), encode = "multipart" );
+
+                  if (r$status_code != 200)
+                  {
+                    stop(content(r, "text"))
+                  }
+
+                  result <- content(r, "text")
+
+                  resultJSON <- fromJSON(result)
+
+                  osmResult <- new_OSMResult(resultJSON)
+
+                  return(osmResult)
+                },
+                assign.env = me)
+
+  return(me)
+}
+
+#'
+#' Constructor for the OSMResult class.
+#'
+#' @description This class extracts data received from JSON OSM simulate() calls to allow later scriptResult conversion
+#'
+#' @return an S3 OSMResult instance
+#'
+#'  TODO update documentation here
+#'
+#' @details
+#'
+#' The class contains the following methods: \cr
+#' \itemize{
+#'
+#' \item \bold{AggregateResults()} \cr
+#' Aggregates results to return only the average value for all plots \cr
+#'
+#' }
+#'
+#' @export
+new_OSMResult <- function(resultJSON)
+{
+  me <- new.env(parent = emptyenv())
+  class(me) <- c("OSMResult")
+  me$dataSet <- read.csv(text = resultJSON$csvReport)
+  me$nbRealizations <- resultJSON$nbRealizations
+  me$nbPlots <- resultJSON$nbPlots
+  me$climateChangeScenario <- resultJSON$climateChangeScenario
+  me$growthModel <- resultJSON$growthModel
+  return (me)
+
+  delayedAssign("AggregateResults",
+                function() {
+                  return (aggregate(Value~Year+SpeciesGroup+OutputType, me$dataSet, FUN="mean"))
+                },
+                assign.env = me)
 }
--- tests/OSMExample.R
+++ tests/OSMExample.R
@@ -1,54 +1,16 @@
 rm(list=ls())
-#
-# df <- read.csv("./tests/OSM_ThreeStandList.csv")
-#
-# SurveyIDList <- unique(df$SurveyID)
-#
-#
-# myCSVString <- convertDataFrameToCSVString(df, SurveyIDList[1])
-#
-#
-# (v <- structure(10*(5:8), names = LETTERS[1:4]))
-# f2 <- function(x, y) outer(rep(x, length.out = 3), y)
-# (a2 <- sapply(v, f2, y = 2*(1:5), simplify = "array"))
-
-#OSMThreeStandList <- read.csv("./tests/OSM_ThreeStandList.csv")
-#save(OSMThreeStandList, file = "./data/OSM_ThreeStandList.RData")
-
-#csvDataFileName <- "./tests/OSM_ThreeStandList.csv"
-#csvData <- readChar(csvDataFileName, file.info(csvDataFileName)$size)
 
 library(Capsis4R)
-library(httr)
-library(jsonlite)
+#library(jsonlite)
 
 data("OSMThreeStandList")
-csvData <- convertDataFrameToCSVString(OSMThreeStandList)
 
-outputRequestFileName <- "./tests/outputRequestList.json"
-outputRequest <- readChar(outputRequestFileName, file.info(outputRequestFileName)$size)
+osm <- new_OSMClass("https://localhost:7032")
+outputRequestList <- new_OSMOutputRequestList()
 
-jsonTest <- fromJSON(outputRequest)
+outputRequestList$addOutputRequest("Alive", "Volume", list(Coniferous = c("BF", "LX"), Broadleaved = c("BE", "ST")))
 
-or <- list()
-or[["requestType"]] <- list(statusClass = "Alive", variable = "Volume")
-or[["aggregationPatterns"]] <- list(Coniferous = c("BF", "LX"), Broadleaved = c("BE", "ST"))
+df <- osm$Simulate(OSMThreeStandList, outputRequestList, "Acadian", 10, 2);
 
-orList <- list()
-orList[[1]] <- or
+print(df)
 
-orListString <- toJSON(orList)
-
-r <- POST( "https://localhost:7032/OSMSimulation/Simulate", query = list(years = "10", variant = "Acadian", ypc = "2"), body = list(data=csvData, output=orListString), encode = "multipart" );
-#r <- POST( "https://localhost:7032/OSMSimulation/Simulate", query = list(years = "10", variant = "Acadian", ypc = "2"), body = list(data=csvData), encode = "multipart" );
-
-if (r$status_code == 200)
-{
-  result <- content(r, "text")
-
-  resultJSON <- fromJSON(result)
-
-  df <- read.csv(text = resultJSON$csvReport)
-
-  print(df)
-}
